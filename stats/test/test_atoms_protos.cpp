27e78904d3eb8fe99af77d73db2905fc8e04b172
Vova Sharaienko <sharaienko@google.com>
2025-10-02 02:04:43 +0000

    [atoms.proto] Added tests for generic vendor atoms
    Flag: EXEMPT DOCS_ONLY
    Bug: 445776142
    Test: atest PlatformAtomsTest
    Change-Id: Id51d1488126065c92bae0794204bfd59aa255600

--- stats/test/test_atoms_protos.cpp
+++ stats/test/test_atoms_protos.cpp
@@ -1,25 +1,25 @@
 /*
-* Copyright (C) 2025, The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2025, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-#include <gtest/gtest.h>
 #include <google/protobuf/descriptor.h>
-
-#include "frameworks/proto_logging/stats/atoms.pb.h"
+#include <gtest/gtest.h>
 
 #include <vector>
+
+#include "frameworks/proto_logging/stats/atoms.pb.h"
 
 namespace android {
 namespace platform_protos {
@@ -38,7 +38,6 @@ void validateMessageFieldsCount(const FieldDescriptor& field) {
 }
 
 TEST(PlatformAtomsTests, AtomMaxFieldNumberTest) {
-
     auto atom = Atom::descriptor();
     // Regular field atoms in Atom
     for (int i = 0; i < atom->field_count(); i++) {
@@ -54,5 +53,5 @@ TEST(PlatformAtomsTests, AtomMaxFieldNumberTest) {
     }
 }
 
-} // namespace platform_protos
-} // namespace android
+}  // namespace platform_protos
+}  // namespace android
